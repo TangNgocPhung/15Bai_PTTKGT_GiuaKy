e58fe60d51ed609a198042e8f26cb9673764cb78
TangNgocPhung <99860033+TangNgocPhung@users.noreply.github.com>
2022-04-20 11:50:48 +0700

    Add files via upload

---
+++ BT10_ChuoiChungDaiNhat/Source.cpp
@@ -0,0 +1,35 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int m, n;
+char a[12309];
+char b[12309];
+int F[2309][2309];
+bool FF[2309][2309];
+
+int f(int m, int n) {
+	if (m == 0 || n == 0) return 0;
+	if (FF[m][n]) return F[m][n];
+
+	int r;
+	if (a[m] == b[n])
+		r = f(m - 1, n - 1) + 1;
+	else
+		r = max(f(m - 1, n), f(m, n - 1));
+	FF[m][n] = true;
+	F[m][n] = r;
+	return r;
+}
+
+int main() {
+	cout << "Ban chon testcase nao tu 1 den 10: "; string s; cin >> s;
+	string fn = "Testcase" + s + ".txt";
+	ifstream cin(fn);
+	if (cin.is_open())
+	{
+		cin >> a >> b;
+		m = strlen(a);
+		n = strlen(b);
+		printf("%d\n", f(m, n));
+	}
+	else cout << "Khong mo duoc File";
+}
